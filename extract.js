b19604f452562d1a6e7cc3a019fead867d412e68
Tom Porter <tom.port@yahoo.co.uk>
2022-09-28 14:53:03 +0100

    New file created with Dave to help understand

--- extract.js
+++ extract.js
@@ -1,46 +1,34 @@
-
-
 import fs from 'fs';
 let text = fs.readFileSync("sample.txt", "utf-8")
 
 console.log("read", text.length, "characters.");
 
+let regex = /(?<person>[a-zA-Z0-9_.]+)@((?<domain>(?:[a-zA-Z0-9_.]+\.)+\w+))/gm
+let domains = {};
 // example of information gathered
 let lineCount = 0;
-let softwireCount = 0;
-let softwire = "@softwire.com";
-let softwireLength = softwire.length;
+
 text.split("\n").forEach(
     processOneLine
 )
 
 console.log(`Processed ${lineCount} lines.`);
-console.log(softwireCount);
+
+Object.entries(domains).forEach( 
+    ( [k,v] ) => console.log(k, v)
+);
+
 // extract information from one line
 // and update global variables
 function processOneLine(line) {
     // example of gathering information
     lineCount++;
-    let softwirePosition = line.indexOf(softwire);
-    // add code here to find emails in the line 
-
-    while (softwirePosition >= 0) {
-        softwireCount++
-        softwirePosition = (line.indexOf(softwire, softwirePosition + softwireLength));
+    
+    for (const match of line.matchAll(regex)) {
+        domains[match.groups.domain] ||= 0;
+        domains[match.groups.domain]++;
     }
+
+
+    // add code here to find emails in the line 
 }
-
-let corndelRegex = /@corndel\.com/gm;
-let corndelFound = text.match(corndelRegex);
-let corndelResult = corndelFound.length;
-console.log(`There are ${corndelResult} addresses with the Corndel domain.`);
-
-let softwireRegex = /@softwire\.com/gm;
-let softwireFound = text.match(softwireRegex);
-let softwireResult = softwireFound.length;
-console.log(`There are ${softwireResult} addresses with the Softwire domain.`);
-
-let gmailRegex = /@gmail\.com/gm;
-let gmailFound = text.match(gmailRegex);
-let gmailResult = gmailFound.length;
-console.log(`There are ${gmailResult} addresses with the gmail domain.`);
